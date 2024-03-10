1f4f427107bdf14879a837febbd76919a7800ee9
MohamadSadem0 <mohamad1.sadem@gmail.com>
2024-03-10 07:57:34 +0200

    .

--- front-end/scripts/main.js
+++ front-end/scripts/main.js
@@ -5,82 +5,63 @@ function setGaugeValue(gauge, value) {
     return;
   }
 
-  gauge.querySelector(".gauge__fill").style.transform = `rotate(${
-    1 / 2
-  }turn)`;
+  gauge.querySelector(".gauge__fill").style.transform = `rotate(${1 / 2}turn)`;
 
   ///note::change it to the currency
-  gauge.querySelector(".gauge__cover").textContent = `${Math.round(
-    1 * 100
-  )}%`;
+  gauge.querySelector(".gauge__cover").textContent = `${Math.round(1 * 100)}%`;
 }
 
 setGaugeValue(gaugeElement, 0.3);
 
+var options = {
+  series: [
+    {
+      name: "Net Profit",
+      data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
+    },
+    {
+      name: "Revenue",
+      data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
+    },
+  ],
+  chart: {
+    type: "bar",
+    height: 350,
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false,
+      columnWidth: "55%",
+      endingShape: "rounded",
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    show: true,
+    width: 2,
+    colors: ["transparent"],
+  },
+  xaxis: {
+    categories: ["Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
+  },
+  yaxis: {
+    title: {
+      text: "$ (thousands)",
+    },
+  },
+  fill: {
+    opacity: 1,
+  },
+  tooltip: {
+    y: {
+      formatter: function (val) {
+        return "$ " + val + " thousands";
+      },
+    },
+  },
+};
 
-        var options = {
-          series: [
-            {
-              name: "Net Profit",
-              data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
-            },
-            {
-              name: "Revenue",
-              data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
-            },
-          
-          ],
-          chart: {
-            type: "bar",
-            height: 350,
-          },
-          plotOptions: {
-            bar: {
-              horizontal: false,
-              columnWidth: "55%",
-              endingShape: "rounded",
-            },
-          },
-          dataLabels: {
-            enabled: false,
-          },
-          stroke: {
-            show: true,
-            width: 2,
-            colors: ["transparent"],
-          },
-          xaxis: {
-            categories: [
-              "Feb",
-              "Mar",
-              "Apr",
-              "May",
-              "Jun",
-              "Jul",
-              "Aug",
-              "Sep",
-              "Oct",
-            ],
-          },
-          yaxis: {
-            title: {
-              text: "$ (thousands)",
-            },
-          },
-          fill: {
-            opacity: 1,
-          },
-          tooltip: {
-            y: {
-              formatter: function (val) {
-                return "$ " + val + " thousands";
-              },
-            },
-          },
-        };
-
-        var chart = new ApexCharts(document.querySelector("#chart"), options);
-        chart.render();
-      
-
-
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
